07b9a1053d15cc6987187c6528e0921d0b7f7052
Ivan Farid Espadas <ivanfaridespadas@gmail.com>
2026-01-17 08:10:29 -0600

    style: update social links to only Facebook and Instagram

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -23,8 +23,7 @@ const footerLinks = {
 }
 
 const socialLinks = [
-  { name: 'Twitter', icon: 'twitter', url: 'https://twitter.com' },
-  { name: 'YouTube', icon: 'youtube', url: 'https://youtube.com' },
+  { name: 'Facebook', icon: 'facebook', url: 'https://facebook.com' },
   { name: 'Instagram', icon: 'instagram', url: 'https://instagram.com' },
 ]
 </script>
@@ -65,7 +64,7 @@ const socialLinks = [
               :aria-label="social.name"
             >
               <svg
-                v-if="social.icon === 'twitter'"
+                v-if="social.icon === 'facebook'"
                 xmlns="http://www.w3.org/2000/svg"
                 width="20"
                 height="20"
@@ -73,19 +72,7 @@ const socialLinks = [
                 fill="currentColor"
               >
                 <path
-                  d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
-                />
-              </svg>
-              <svg
-                v-if="social.icon === 'youtube'"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-              >
-                <path
-                  d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"
+                  d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953h-1.514c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"
                 />
               </svg>
               <svg
